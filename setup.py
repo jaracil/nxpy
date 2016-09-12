a8c905ba3941cc173dad34840a597a5389cae989
Javier Sancho <jsf@jsancho.org>
2016-09-12 10:40:30 +0200

    Version changed to 1.3.4 and python 2.6 support added

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.rst') as f:
 
 setup(
     name='pynexus',
-    version='1.3.2',
+    version='1.3.4',
     description='A Python library for easy playing with Nexus',
     long_description=long_description,
     url='https://github.com/jaracil/nxpy',
@@ -39,6 +39,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
