8a935a33ed263b5b5400913c7834d941900cc16b
Javier Sancho <jsf@jsancho.org>
2016-09-15 12:13:31 +0200

    Version changed to 1.3.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.rst') as f:
 
 setup(
     name='pynexus',
-    version='1.3.4',
+    version='1.3.5',
     description='A Python library for easy playing with Nexus',
     long_description=long_description,
     url='https://github.com/jaracil/nxpy',
